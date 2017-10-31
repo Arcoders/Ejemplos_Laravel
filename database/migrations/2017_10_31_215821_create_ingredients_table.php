d6c4e33ab77229dfd52d7bfed434ec645de4a639
arcoders <anonismaa@gmail.com>
2017-10-31 23:18:09 +0100

    migrate tabla pizzas y ingredientes

---
+++ database/migrations/2017_10_31_215821_create_ingredients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name');
+            $table->float('price', 10, 2);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::dropIfExistes('ingredientes');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+    }
+}
