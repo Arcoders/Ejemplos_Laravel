5a567ba17eafdd3e807c5b98ed0fcf422618699f
arcoders <anonismaa@gmail.com>
2017-11-20 23:11:39 +0100

    Rutas Api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:api')->get('/pizzas', function()
+{
+   return \App\Pizza::with('ingredients')->find(1) ?: [];
+});
